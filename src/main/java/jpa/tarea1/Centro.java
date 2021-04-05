f7e83fdcf6c0c5d786bd622360719c0d22dc52ca
Alumno SII <cabrerizo@uma.es>
2021-04-05 09:45:05 -0700

    se ha añadido la clase Centro

---
+++ src/main/java/jpa/tarea1/Centro.java
@@ -0,0 +1,16 @@
+package jpa.tarea1;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Encuesta
+ *
+ */
+@Entity
+public class Centro {
+	
+	public Centro() {
+		
+	}
+}
