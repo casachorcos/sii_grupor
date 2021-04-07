d96460be4d2b474cc076b1bad6962cb4568513b8
casachorcos <nicocasamayor@uma.es>
2021-04-07 09:37:52 -0700

    Creación de expediente

---
+++ src/main/java/jpa/tarea1/Expediente.java
@@ -0,0 +1,23 @@
+package jpa.tarea1;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Expediente
+ *
+ */
+@Entity
+
+public class Expediente {
+
+	private int cr_fb;
+	private int cr_ob;
+	private int cr_op;
+	private int cr_cf;
+	
+
+	public Expediente() {
+		super();
+	}
+   
+}
