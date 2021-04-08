079593f09f493697f0a3d7c408f770069d4a89ec
nicocasamayor <nicocasamayor@uma.es>
2021-04-08 11:24:08 +0200

    Terminada Clase a falta de relaciones

--- src/main/java/jpa/tarea1/Clase.java
+++ src/main/java/jpa/tarea1/Clase.java
@@ -3,18 +3,20 @@ package jpa.tarea1;
 import java.util.Date;
 import javax.persistence.*;
 
+
+
 @Entity
 public class Clase {
 
-	
-	private String dia;
-	private Date horaInicio;
+	@EmbeddedId
+	private ClaseId id;
+	@Column (name = "Hora_fin")
+	@Temporal(TemporalType.DATE)
 	private Date horaFin;
 	
 	public Clase(String d, Date hi, Date hf) {
 		super();
-		dia = d;
-		horaInicio = hi;
+		id = new ClaseId(d,hi);
 		horaFin = hf;
 	}
 	
@@ -22,21 +24,6 @@ public class Clase {
 		
 	}
 
-	public String getDia() {
-		return dia;
-	}
-
-	public void setDia(String dia) {
-		this.dia = dia;
-	}
-
-	public Date getHoraInicio() {
-		return horaInicio;
-	}
-
-	public void setHoraInicio(Date horaInicio) {
-		this.horaInicio = horaInicio;
-	}
 
 	public Date getHoraFin() {
 		return horaFin;
@@ -50,9 +37,8 @@ public class Clase {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((dia == null) ? 0 : dia.hashCode());
 		result = prime * result + ((horaFin == null) ? 0 : horaFin.hashCode());
-		result = prime * result + ((horaInicio == null) ? 0 : horaInicio.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -65,29 +51,23 @@ public class Clase {
 		if (getClass() != obj.getClass())
 			return false;
 		Clase other = (Clase) obj;
-		if (dia == null) {
-			if (other.dia != null)
-				return false;
-		} else if (!dia.equals(other.dia))
-			return false;
 		if (horaFin == null) {
 			if (other.horaFin != null)
 				return false;
 		} else if (!horaFin.equals(other.horaFin))
 			return false;
-		if (horaInicio == null) {
-			if (other.horaInicio != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!horaInicio.equals(other.horaInicio))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Clase [dia=" + dia + ", horaInicio=" + horaInicio + ", horaFin=" + horaFin + "]";
+		return "Clase [id=" + id + ", horaFin=" + horaFin + "]";
 	}
-	
 	
 	
 }
