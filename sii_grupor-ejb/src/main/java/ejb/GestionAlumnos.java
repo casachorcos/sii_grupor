b66d1ebf5a4b49b9219eae8cad611bfe0d1af9e8
Alumno SII <cabrerizo@uma.es>
2021-05-22 10:10:07 -0700

    Paso de la T2 a T3

---
+++ sii_grupor-ejb/src/main/java/ejb/GestionAlumnos.java
@@ -0,0 +1,18 @@
+package ejb;
+
+import ejb.excepciones.AlumnoNoEncontradoException;
+import ejb.excepciones.TrazabilidadException;
+import jpa.entidades.Alumno;
+import javax.ejb.Local;
+
+@Local
+public interface GestionAlumnos {
+	
+	/**
+	 * Actualiza un alumno en la base de datos.
+	 * Si no se encuentra al alumno en la base de datos, lanza la excepci�n AlumnoNoEncontradoException.
+	 * @param alumno Alumno que se quiere actualizar.
+	 */
+	public void actualizarAlumno(Alumno alumno) throws TrazabilidadException;
+
+}
---
+++ sii_grupor-ejb/src/main/java/jpa/entidades/Optativa.java
@@ -0,0 +1,73 @@
+package jpa.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ * Entity implementation class for Entity: Optativa
+ *
+ */
+@Entity
+public class Optativa extends Asignatura implements Serializable {
+
+	private static final long serialVersionUID = 11L;
+	@Column (name = "Plazas", nullable = false)
+	private int plazas;
+	@Column (name = "Mención", nullable = false, length = 50)
+	private String mencion;
+
+	public Optativa() {
+		super();
+	}
+
+	public int getPlazas() {
+		return plazas;
+	}
+
+	public void setPlazas(int plazas) {
+		this.plazas = plazas;
+	}
+
+	public String getMencion() {
+		return mencion;
+	}
+
+	public void setMencion(String mencion) {
+		this.mencion = mencion;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((mencion == null) ? 0 : mencion.hashCode());
+		result = prime * result + plazas;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Optativa other = (Optativa) obj;
+		if (mencion == null) {
+			if (other.mencion != null)
+				return false;
+		} else if (!mencion.equals(other.mencion))
+			return false;
+		if (plazas != other.plazas)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Optativa [plazas=" + plazas + ", mencion=" + mencion + "]";
+	}
+}
