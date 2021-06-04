2a9dc3b9b933e9542d06dc2cbad30a402520171e
AlejandroQP <alexquintana@uma.es>
2021-06-04 07:28:11 -0700

    Añadido alumno y correccion de menu

---
+++ sii_grupor-war/src/main/java/es/uma/informatica/sii/backing/Alumnos.java
@@ -0,0 +1,81 @@
+package es.uma.informatica.sii.backing;
+
+import jpa.entidades.Alumno;
+import ejb.excepciones.AlumnoNoEncontradoException;
+import ejb.excepciones.TrazabilidadException;
+import ejb.GestionAlumnos;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named(value = "alumnos")
+@RequestScoped
+public class Alumnos {
+
+    public static enum Modo {
+        MODIFICAR,
+        INSERTAR,
+        NOACCION
+    };
+
+    @Inject
+    private GestionAlumnos gestionAlumnos;
+
+    private Alumno alumno;
+    private Modo modo;
+
+    public Alumnos() {
+        alumno = new Alumno();
+        modo = Modo.NOACCION;
+    }
+
+    public Modo getModo() {
+        return modo;
+    }
+
+    public void setModo(Modo modo) {
+        this.modo = modo;
+    }
+
+    public String getAccion() {
+        switch (modo) {
+            case MODIFICAR:
+                return "Modificar";
+            case INSERTAR:
+                return "Insertar";
+
+        }
+        return null;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public String modificar(Alumno a) {
+        alumno = a;
+        setModo(Modo.MODIFICAR);
+        return "edicionContacto.xhtml";
+    }
+
+    public String ejecutarAccion() {
+        try {
+            switch (modo) {
+                case MODIFICAR:
+                    gestionAlumnos.actualizarAlumno(alumno);
+                    break;
+            }
+            return "alumnos.xhtml";
+        } catch (TrazabilidadException e) {
+            return "login.xhtml";
+		}
+    }
+    
+    public String menu() {
+        return "menuSecretaria.xhtml";
+    }
+}
